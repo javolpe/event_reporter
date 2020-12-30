f8b090b08ad5275b9a95df0f82046845d02dca96
Jacob Volpe <volpe.jacob@gmail.com>
2020-12-30 16:28:50 -0700

    welcome message, get input

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -34,22 +34,27 @@ class Reporter
       end
   end
 
-  def data_query
+  def welcome_message
     holder = []
     CSV.foreach("#{load_file}", headers: true, header_converters: :symbol){|row| holder << row}
     
     contents = clean_initial_data(holder)
-    
-    puts "******************************************"
-    puts "queue count #{@queue.length}"
-    puts "******************************************"
-    puts "  This program works by the user entering commands and the program will then narrow down the list of attendees based on user input."
-    puts "  To start narrowing down from the full attendees list enter 'find <attribute> <criteria>' (ie find first_name john)"
-    puts "  Your queue"
-    
+    headers = contents[0].headers[1..-1].map{|header| header.to_s} 
+    puts "*********************************************************************"
+    puts "This program works by the user entering commands and the program will then narrow down the list of attendees based on user input."
+    puts "To start narrowing down from the full attendees list enter 'find <attribute> <criteria>' (ie: find first_name john).  Attributes are:"
+    p     headers
+    puts ""
+    puts "After that a queue will be created of everyone who matches that specific criteria."  
+    puts "If you run 'find <attribute> <criteria>' twice it will include everyone who matches BOTH sets of criteria"
+    puts ""
+    puts "Queue commands: queue count, queue clear, queue print, queue print by <attribute>, queue save to <filename.csv>, queue export html <filenmae.csv>"
+    puts "At any time you can enter 'help' to see this list again or 'help <command>' (ie: help queue print) for an explanation of commands."
+    puts "Please enter your command below >>>"
+    command = gets.chomp
     # query = @find.find_method(contents)
     # query = @queue.start(query)
-    binding.pry
+    # run_command(command, contents)
   end
 
 
@@ -66,7 +71,17 @@ class Reporter
     end 
   end
 
+  def run_command(command, contents)
+    execute = command.split(" ", 3)
+    if execute.first == "find"
+      @find.find_method(execute, contents)
+    end
+    
+  end
+
+
+
 end
 
 cat = Reporter.new
-cat.data_query
+cat.welcome_message
